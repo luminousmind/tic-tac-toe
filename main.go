82b1cc5c40f44aeda7025640bb8ddc190f59423b
luminousmind <193550240+luminousmind@users.noreply.github.com>
2025-01-04 13:54:50 +1100

    Add gameboard/printing system and game-start explanation

--- main.go
+++ main.go
@@ -2,6 +2,40 @@ package main
 
 import "fmt"
 
+var gameboard [3][3]rune = [3][3]rune{
+	{' ', ' ', ' '},
+	{' ', ' ', ' '},
+	{' ', ' ', ' '},
+}
+
+func printBoard() {
+	for y := 0; y < 3; y++ {
+		for x := 0; x < 3; x++ {
+			fmt.Printf("%c ", gameboard[x][y])
+		}
+
+		fmt.Println()
+	}
+}
+
+func gameStart() {
+	fmt.Println("Tic-tac-toe is a simple boardgame in which two players alternate marking the spaces in three-by-three grid with X and O.")
+	fmt.Println("In a brief explanation of the rules of Tic-tac-toe:")
+	fmt.Println()
+	fmt.Println("The rules of Tic-tac-toe:")
+	fmt.Println("1. The game is played on a three-by-three grid.")
+	fmt.Println("2. Two players exchange turns marking a square on the grid with X or O.")
+	fmt.Println("3. The first player to get three in a row horizontally, vertically, or diagonally wins.")
+	fmt.Println("4. If all squares are filled without a winner, it is a draw.")
+	fmt.Println()
+	fmt.Println("Press enter to continue.")
+	fmt.Scanln()
+}
+
 func main() {
-	fmt.Println("Hello World")
+	gameboard[1][1] = 'X'
+
+	gameStart()
+
+	printBoard()
 }
