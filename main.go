a654cb3a6476286f2152869272b2439457630a74
luminousmind <193550240+luminousmind@users.noreply.github.com>
2025-01-04 15:35:47 +1100

    Implement script to get move coordinates

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 var gameboard [3][3]rune = [3][3]rune{
 	{' ', ' ', ' '},
 	{' ', ' ', ' '},
 	{' ', ' ', ' '},
 }
+
+var playerTurn rune = 'X'
 
 func printBoard() {
 	for y := 0; y < 3; y++ {
@@ -32,10 +37,47 @@ func gameStart() {
 	fmt.Scanln()
 }
 
-func main() {
-	gameboard[1][1] = 'X'
+func getCord(axis rune) int {
+	fmt.Printf("Enter the %c coordinate to place an %c: ", axis, playerTurn)
 
+	var input string
+
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		fmt.Printf("Error reading input: %v\n", err)
+		return -1
+	}
+
+	num, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Printf("Error converting string to int: %v\n", err)
+		return -1
+	}
+
+	return num
+}
+
+func getMove() [2]int {
+	if playerTurn == 'X' {
+		fmt.Println("It is Player 1's turn.")
+	} else {
+		fmt.Println("It is Player 2's turn.")
+	}
+
+	var coords [2]int
+
+	coords[0] = getCord('X')
+	coords[1] = getCord('Y')
+
+	return coords
+}
+
+func main() {
 	gameStart()
+
+	move := getMove()
+
+	fmt.Println(move)
 
 	printBoard()
 }
